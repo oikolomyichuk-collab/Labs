3c10a5e7b2c64730a088c72d4fbab4054b1b87ab
Yokai <mysicgame@gmail.com>
2026-04-14 14:56:14 +0300

    Виконано практичну роботу 3

---
+++ Task3/ConsoleApp1/DB.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    public class DB
+    {
+
+        Storage storage;
+        public IRepository<Detail> repoDetail;
+        public IRepository<Production> repoProduction;
+        public IRepository<Operation> repoOperation;
+        public DB()
+        {
+            storage = new Storage();
+            storage.Database.EnsureCreated();
+            repoDetail = new Repository<Detail>(storage);
+            repoProduction = new Repository<Production>(storage);
+            repoOperation = new Repository<Operation>(storage);
+        }
+    }
+}
